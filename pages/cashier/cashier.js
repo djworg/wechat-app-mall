2af8a6083f31a12e79b75e45a80be9ff501d6a99
junwen.deng <junwen.deng@yeepay.com>
2019-02-23 10:47:36 +0800

    理发 完成所有功能

--- pages/cashier/cashier.js
+++ pages/cashier/cashier.js
@@ -18,51 +18,87 @@ Page({
   balancePay: function (orderId, amount) {
     var that = this;
     WXAPI.orderPay(orderId, wx.getStorageSync('token')).then(function (res) {
+      wx.hideLoading();
       that.onShow();
     });
+  },
+
+/**
+ * 获得微信应该支付多少钱，
+ * 订单金额 - 账户余额 = 微信应付
+ */
+  getWxNeedPay:function(amount){
+    var wxNeedPay;
+    var loginToken = wx.getStorageSync('token'); // 用户登录 token
+    var balance = wx.getStorageSync('balance');
+   
+    var temp = amount - balance;
+    if (temp > 0) {
+    //余额不足
+      wxNeedPay = temp.toFixed(2);
+    } else {
+      //余额充足
+      wxNeedPay = 0;
+    }
+    return wxNeedPay;
   },
   /**
       * 支付
       */
   comboPay:function (orderId, amount) {
+    console.log("orderId------:"+orderId);
     var that = this;
-    var loginToken = wx.getStorageSync('token'); // 用户登录 token
-    WXAPI.userAmount(loginToken).then(function (res) {
-      if (res.code == 0) {
-        var temp = amount - res.data.balance;
-        if (temp > 0) {//余额不足
-          //先充值，再支付
-          wxpay.wxpay(app, temp, orderId, "");
-          //使用余额支付
-          that.balancePay(orderId, amount);
-        } else {
-          //直接使用余额支付
-          that.balancePay(orderId, amount);
-        }
-      } else {
-        wx.showModal({
-          title: '错误',
-          content: '无法获取用户资金信息',
-          showCancel: false
-        })
-      }
-    });
+    var wxNeedPay = that.getWxNeedPay(amount);
+    if (wxNeedPay + '1' == '1' || undefined == wxNeedPay) {
+      wx.showModal({
+        title: '错误',
+        content: '无法获取用户资金信息',
+        showCancel: false
+      });
+      wx.hideLoading();
+      return;
+    }
+    console.log(wxNeedPay+"---"+amount);
+    if(wxNeedPay > 0){
+      
+      //先充值，再支付
+      wxpay.wxpay(app, wxNeedPay, orderId, "");
+     
+    }
+    //使用余额支付
+    that.balancePay(orderId, amount);
+   
+    
+    
+    //TODO 重定向支付成功页面
+
   },
   /**
- * 使用余额支付，先创建订单   改商品每件单价为1元
+ * 使用余额支付，先创建订单   
  */
-  createOrderAndPay: function (goodsId, amount) {
+  createOrderAndPay: function (goodsId, amount, staff) {
     wx.showLoading();
     var that = this;
-    var loginToken = wx.getStorageSync('token') // 用户登录 token
+    var loginToken = wx.getStorageSync('token'); // 用户登录 token
     var num = parseInt(amount * 10);
-      var that = this;
-      var remark = "使用余额支付"; // 备注信息
-      var postData = {
-        token: loginToken,
-        goodsJsonStr: '[{"goodsId": ' + goodsId + ',"number":' + num + '}]',
-        remark: remark
-      };
+    var wxNeedPay = that.getWxNeedPay(amount);
+    console.log(wxNeedPay+"---"+amount);
+    if (wxNeedPay + '1' == '1' || undefined == wxNeedPay) {
+      wx.showModal({
+        title: '错误',
+        content: '无法获取用户资金信息',
+        showCancel: false
+      });
+      wx.hideLoading();
+      return;
+    }
+    var remark = "发型师工号为："+staff+"，微信支付："+wxNeedPay+"，余额支付："+(amount-wxNeedPay);
+    console.log(remark); // 备注信息
+    var postData = {
+      token: loginToken,
+      goodsJsonStr: '[{"goodsId": ' + goodsId + ',"number":' + num + '}]',
+      remark: remark
+    };
      WXAPI.orderCreate(postData).then(function (res) {
         if (res.code != 0) {
             wx.hideLoading();
@@ -73,7 +109,7 @@ Page({
             })
             return;
         }
-        console.log(res.data.id+"_------"+amount);
+
        that.comboPay(res.data.id, amount);
       });
       
@@ -82,6 +118,7 @@ Page({
     console.log(e)
     var that = this;
     var amount = e.detail.value.amount;
+    var staff = e.detail.value.staff;
     var type = e.detail.value.type;
     if (amount == "" || amount * 1 < 0) {
       wx.showModal({
@@ -91,12 +128,19 @@ Page({
       })
       return
     }
+    if (staff == "" || staff + '1' == '1' || undefined == staff) {
+      wx.showModal({
+        title: '错误',
+        content: '请选择发型师',
+        showCancel: false
+      })
+      return
+    }
     switch(type){
       case 'pay':{
         // 微信支付
-        var goodsId = '117861';
-        that.createOrderAndPay(goodsId, amount);
-        
+        var goodsId = that.getGoodsId(staff);
+        that.createOrderAndPay(goodsId, amount, staff);
       };
       break;
       case 'recharge':{
@@ -108,6 +152,15 @@ Page({
     }
     
   },
+  getGoodsId:function(staff){
+    var goodsId;
+    if(staff == '01'){
+      goodsId = '117861';
+    }else {
+      goodsId = '118867';
+    }
+    return goodsId;
+  },
   
   /**
    * 获得用户余额
@@ -116,37 +169,46 @@ Page({
     var that = this;
     WXAPI.userAmount(wx.getStorageSync('token')).then(function (res) {
       if (res.code == 0) {
+        wx.setStorageSync('balance', res.data.balance);
         that.setData({
           balance: res.data.balance.toFixed(2),
           freeze: res.data.freeze.toFixed(2),
           score: res.data.score
         });
+      } else {
+        wx.showModal({
+          title: '错误',
+          content: '无法获取用户资金信息',
+          showCancel: false
+        });
       }
     })
   },
-  
-
-
-
-
 /**
  * 获得充值活动
  */
-  getRechargeActivity(){
+  getRechargeRule(){
     var that = this;
     // 获取充值活动优惠
-    WXAPI.queryConfig({
-      key: 'recharge-config'
-    }).then(function (res) {
-      if (res.code == 0) {
-        // var arr = res.data.value;
-        // that.setData({
-        //   rechargeDic: arr,
-        // });
-      }
-    })
+    WXAPI.rechargeRule().then(function(res){
+        if(res.code == 0){
+          console.log(res.data);
+          var arr = res.data;
+          that.setData({
+            rechargeDic: arr,
+            confine:arr[0].confine,
+            send:arr[0].send
+          });
+        
+        }else{
+          wx.showModal({
+            title: '错误',
+            content: '无法获得充值优惠',
+            showCancel: false
+          });
+        }
+    });
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -155,7 +217,6 @@ Page({
     //获得当前账户余额
     
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -168,7 +229,7 @@ Page({
    */
   onShow: function () {
     this.getUserAmount();
-    this.getRechargeActivity();
+    this.getRechargeRule();
   },
 
   /**
